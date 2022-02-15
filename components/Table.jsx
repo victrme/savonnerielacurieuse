fbebafe25a5abe7cf3dfba67a08d0f3c91438cc4
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-02-15 22:04:57 +0100

    ✨ Added event table component

---
+++ components/Table.jsx
@@ -0,0 +1,51 @@
+const Table = (params) => {
+	let row = []
+	const fullTable = []
+	const maxRows = Math.max(...Object.values(params.events).map((a) => a.length))
+
+	for (let i = 0; i < maxRows; i++) {
+		Object.values(params.events).forEach((elem) => row.push(elem[i] ? elem[i] : {}))
+		fullTable.push(row)
+		row = []
+	}
+
+	return (
+		<div className='table_wrap'>
+			<table>
+				<thead>
+					<tr>
+						<td>Mars</td>
+						<td>avril</td>
+						<td>mai</td>
+						<td>juin</td>
+						<td>juillet</td>
+						<td>août</td>
+						<td>septembre</td>
+						<td>octobre</td>
+						<td>novembre</td>
+						<td>décembre</td>
+					</tr>
+				</thead>
+				<tbody>
+					{fullTable.map((row, i) => (
+						<tr key={i}>
+							{row.map(({ nom, date, horaire, adresse, note }) => (
+								<td key={nom}>
+									<span>{date}</span> <br />
+									<span>{horaire}</span> <br />
+									<strong>{nom}</strong>
+									<br />
+									<small>{adresse}</small>
+									<br />
+									{note ? <small>{note}</small> : ''}
+								</td>
+							))}
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	)
+}
+
+export default Table
