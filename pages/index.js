5efb9e46209f2fa789f2358e6281e66afb1b106d
Victor Azevedo <victor.azevedo@mail.com>
2022-03-14 10:54:03 +0100

    🔍 Added Open Graph metadata & jpg to webp

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ const Home = (props) => {
 	return (
 		<>
 			<Head>
+				<title>Savonnerie La Curieuse - Création de savons écologiques</title>
 				<meta
 					name='description'
 					content='Des savons 100% fait main crées à Vollore-Ville (Auvergne) selon la méthode ancestrale de saponification à froid.'
@@ -23,10 +24,27 @@ const Home = (props) => {
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<meta charSet='utf-8' />
 
-				<title>Savonnerie La Curieuse - Création de savons écologiques</title>
-
 				<link rel='apple-touch-icon' sizes='180x180' href='apple-touch-icon.png' />
 				<link rel='icon' href='favicon.ico' />
+
+				<meta property='og:url' content='https://www.savonnerielacurieuse.com/' />
+				<meta property='og:type' content='website' />
+				<meta property='og:title' content='Savonnerie La Curieuse - Création de savons écologiques' />
+				<meta
+					property='og:description'
+					content='Des savons 100% fait main crées à Vollore-Ville (Auvergne) selon la méthode ancestrale de saponification à froid.'
+				/>
+				<meta property='og:image' content='/images/preview.webp' />
+
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta property='twitter:domain' content='savonnerielacurieuse.com' />
+				<meta property='twitter:url' content='https://www.savonnerielacurieuse.com/' />
+				<meta name='twitter:title' content='Savonnerie La Curieuse - Création de savons écologiques' />
+				<meta
+					name='twitter:description'
+					content='Des savons 100% fait main crées à Vollore-Ville (Auvergne) selon la méthode ancestrale de saponification à froid.'
+				/>
+				<meta name='twitter:image' content='/images/preview.webp' />
 			</Head>
 
 			<div className='background'>
@@ -190,8 +208,8 @@ export async function getStaticProps() {
 
 	// blurDataURL
 	const savBlurs = await getBlurDataURL(savPaths)
-	const catBlurs = await getBlurDataURL(categories.map((id) => `/images/categorie/${id}.jpg`))
-	const accBlur = await getBlurDataURL(['/images/banniere/accueil.jpg'])
+	const catBlurs = await getBlurDataURL(categories.map((id) => `/images/categorie/${id}.webp`))
+	const accBlur = await getBlurDataURL(['/images/accueil.webp'])
 
 	const addDataURL = (target, blurs) => target.map((e, i) => ({ ...e, blurDataURL: blurs[i] }))
 
